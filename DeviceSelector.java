1f6b58d717ff98a57572762b48e4a85b12d484fc
Şamil Algül <32276205+samilalgul@users.noreply.github.com>
2017-12-11 20:08:09 +0300

    Add files via upload

---
+++ DeviceSelector.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Samil on 11.12.2017.
+ */
+public class DeviceSelector {
+}
---
+++ JSIoTSoftware.java
@@ -0,0 +1,28 @@
+/**
+ * Created by Samil on 11.12.2017.
+ */
+public class JSIoTSoftware {
+
+    boolean sleep;
+
+    void switchSleepMode(){
+
+    }
+    void switchAwakeMode(){
+
+    }
+    boolean sleepMode(){
+
+        return false;
+
+    }
+
+    void commandSmartphone(){
+
+    }
+
+    void touchButton(){
+
+    }
+
+}
