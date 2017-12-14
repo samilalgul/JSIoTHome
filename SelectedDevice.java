d4c5d994611cdd1cde55bbc84c1af1b6bf79991f
Şamil Algül <32276205+samilalgul@users.noreply.github.com>
2017-12-14 20:22:29 +0300

    Update SelectedDevice.java

--- SelectedDevice.java
+++ SelectedDevice.java
@@ -2,4 +2,47 @@
  * Created by Samil on 11.12.2017.
  */
 public class SelectedDevice {
+
+    String name;
+
+    public SelectedDevice(String name){
+
+        this.name = name;
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    boolean run;
+
+    void turnOn(){
+        run = true;
+        System.out.println(run);
+    }
+
+    void turnOff(){
+
+            run = false;
+
+    }
+
+    public boolean isRun() {
+        return run;
+    }
+
+
+    String deviceStatus(){
+        if (run = true){
+            return "Power On";
+        }
+        else return "Power Off";
+
+    }
 }
